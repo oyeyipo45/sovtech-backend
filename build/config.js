905daefacf2290acab7460bad42acbb9b8606615
damilola oyeyipo <oyeyipo45@gmail.com>
2022-09-26 09:38:29 +0100

    delete build folder

--- build/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: process.env.PORT || 5003,
-    baseUrl: process.env.BASE_URL || 'https://swapi.dev/api',
-};
